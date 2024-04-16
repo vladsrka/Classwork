6499a0375f6bfa47bbb2a1277b64568b8a348d5e
vladsrka <svv.post01@gmail.com>
2024-04-16 21:01:12 +0300

    work

--- Lesson-19/Lesson-19.1/script.js
+++ Lesson-19/Lesson-19.1/script.js
@@ -1,4 +1,7 @@
 "use strict";
-console.log(document.querySelector('h3').textContent)
-console.log(document.querySelector('p').textContent)
-console.log(document.querySelectorAll('li:last-of-type')[3].textContent)
+// console.log(document.querySelector('h3').textContent)
+// console.log(document.querySelector('p').textContent)
+// console.log(document.querySelectorAll('li:last-of-type')[3].textContent)
+// for (let i = 0; i < liElement.lenght; i++) {
+//     liElement[i]
+// }
