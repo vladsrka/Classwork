85f175084b4e2453d4d810e849b2e073c3d1da0d
vladsrka <svv.post01@gmail.com>
2024-04-16 20:33:48 +0300

    work

--- Lesson-19/Lesson-19.1/script.js
+++ Lesson-19/Lesson-19.1/script.js
@@ -1,1 +1,4 @@
 "use strict";
+console.log(document.querySelector('h3').textContent)
+console.log(document.querySelector('p').textContent)
+console.log(document.querySelectorAll('li:last-of-type')[3].textContent)
