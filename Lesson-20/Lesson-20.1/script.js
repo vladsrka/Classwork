8b26e12a0b4bbaa0819dcbd64064718a6eb62963
vladsrka <svv.post01@gmail.com>
2024-04-19 19:32:43 +0300

    work

--- Lesson-20/Lesson-20.1/script.js
+++ Lesson-20/Lesson-20.1/script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+// document.querySelector('div').onclick = function () {
+//     document.querySelector('div').classList.toggle('active')
+// }
+
+const allBlocks = document.querySelectorAll('div');
+
+for (let i = 0; i < allBlocks.length; i++) {
+    allBlocks[i].addEventListener('click', activeBlock);
+}
+
+function activeBlock(event) {
+    console.log(event.target.parentElement.dataset.id);
+
+    const currentBlock = event.target.parentElement.dataset.id;
+    for (let i = 0; i < allBlocks.length; i++) {
+        allBlocks[i].classList.remove('active');
+    }
+    allBlocks[currentBlock - 1].classList.add('active');
+}
+
+document.addEventListener('click', outsideDocument);
+
+function outsideDocument(event) {
+    for (let i = 0; i < allBlocks.length; i++) {
+        if (event.target === allBlocks[i] || allBlocks[i].contains(event.target)) {
+            continue;
+        }
+        allBlocks[i].classList.remove('active');
+    }
+}
