538c77c8be17b7faac2dd4eb7c181397fd06a11d
vladsrka <svv.post01@gmail.com>
2024-04-19 21:14:49 +0300

    work

--- Lesson-20/Lesson-20.3/script.js
+++ Lesson-20/Lesson-20.3/script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+let offSet = 0;
+
+let sliderLine = document.querySelector('.slider-line');
+
+let nextButton = document.querySelector('.slider-next');
+
+let prevButton = document.querySelector('.slider-prev');
+
+nextButton.onclick = function () {
+    offSet += 225;
+    if (offSet > 900) {
+        offSet = 0;
+    }
+    sliderLine.style.left = -offSet + 'px'
+}
+
+prevButton.onclick = function () {
+    offSet -= 225;
+    if (offSet < 0) {
+        offSet = 900;
+    }
+    sliderLine.style.left = -offSet + 'px'
+}
