6f1f12e244f1326999504f9058dd14a3349b2f67
vladsrka <svv.post01@gmail.com>
2024-04-26 20:23:04 +0300

    work

--- Lesson-22/Lesson-22.1/script.js
+++ Lesson-22/Lesson-22.1/script.js
@@ -2,13 +2,21 @@
 
 let inputs = document.querySelectorAll('input');
 let button = document.querySelector('button');
+let header = document.querySelector('h1')
+
 button.addEventListener('click', addError);
 
 function addError() {
+    let error = 0;
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].value == '' || inputs[i].value == null) {
             inputs[i].parentElement.classList.add('error');
+            error += 1;
         }
+    }
+    if (error == 0) {
+        header.innerHTML = `Добро пожаловать ${inputs[0].value} ${inputs[1].value}`;
+        inputs.value = '';
     }
 }
 
