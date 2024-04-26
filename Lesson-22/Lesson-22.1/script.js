e09e378ac01b5323a36eac279ec29b985ef52edf
vladsrka <svv.post01@gmail.com>
2024-04-26 19:52:25 +0300

    work

--- Lesson-22/Lesson-22.1/script.js
+++ Lesson-22/Lesson-22.1/script.js
@@ -1,2 +1,21 @@
 "use strict";
 
+let inputs = document.querySelectorAll('input');
+let button = document.querySelector('button');
+button.addEventListener('click', addError);
+
+function addError() {
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value == '' || inputs[i].value == null) {
+            inputs[i].parentElement.classList.add('error');
+        }
+    }
+}
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('focus', removeError);
+}
+
+function removeError(event) {
+    event.target.parentElement.classList.remove('error');
+}
