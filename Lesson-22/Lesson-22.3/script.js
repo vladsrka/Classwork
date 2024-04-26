0f45bbdfa7c762a249880361d7696bb73fd574bf
vladsrka <svv.post01@gmail.com>
2024-04-26 21:21:24 +0300

    work

--- Lesson-22/Lesson-22.3/script.js
+++ Lesson-22/Lesson-22.3/script.js
@@ -1,5 +1,19 @@
 "use strict";
 
-let string = 'JS';
-console.log(string.toLowerCase())
+// let string = 'JS';
+// console.log(string.toLowerCase())
 
+// let string = 'я учу javascript!';
+// console.log(string.indexOf('javascript'));
+// console.log(string.indexOf('JavaScript'));
+// console.log(string.includes('javascript'));
+// console.log(string.includes('JavaScript'));
+
+function checkString(str, substr) {
+    let correcStr = str.toLowerCase();
+    let correcSubStr = substr.toLowerCase();
+    return correcStr.includes(correcSubStr);
+}
+
+console.log(checkString('я учу javascript!', 'JavaScript'))
+console.log(checkString('я учу javascript!', 'javascript'))
