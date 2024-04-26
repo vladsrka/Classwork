ff77f4cba314020857a8739ed4695d85adb1669f
vladsrka <svv.post01@gmail.com>
2024-04-26 20:58:38 +0300

    work

--- Lesson-22/Lesson-22.2/script.js
+++ Lesson-22/Lesson-22.2/script.js
@@ -1,2 +1,27 @@
 "use strict";
 
+// let num = 379;
+// let math = Math.sqrt(num);
+// console.log(Math.floor(math));
+// console.log(math.toFixed(1));
+// console.log(math.toFixed(2));
+
+// let num = 587;
+// let sqrt = Math.sqrt(num);
+// let obj = {
+//     floor: Math.floor(sqrt),
+//     ceil: Math.ceil(sqrt)
+// };
+// console.log(obj)
+
+// let arr = [];
+// for (let i = 0; i < 10; i++) {
+//     arr[i] = Math.floor(Math.random() * 100);
+// }
+// console.log(arr);
+
+let max = Math.max(4, -2, 5, 19, -130, 0, 10);
+let min = Math.min(4, -2, 5, 19, -130, 0, 10);
+console.log(`Максимальное значение: ${max}\nМинимальное значение: ${min}`);
+
+
