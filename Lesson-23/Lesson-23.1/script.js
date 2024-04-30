2447a65d416fe63eb287830fd4b1ff17c372c5f5
vladsrka <svv.post01@gmail.com>
2024-04-30 21:03:14 +0300

    work

--- Lesson-23/Lesson-23.1/script.js
+++ Lesson-23/Lesson-23.1/script.js
@@ -5,11 +5,13 @@
 //     console.log(str11.startsWith('http://'));
 // }
 
+
 // let str2 = 'https://dfgdfgdfvdv';
 // check2(str2);
 // function check2(str22) {
 //     console.log(str22.startsWith('https://'));
 // }
+
 
 // let str1 = 'https://dfgdfgdfvdv.com';
 // let str2 = 'https://dfgdfgdfvdv.by';
@@ -33,11 +35,64 @@
 //     }
 // }
 
-let str = 'JavaScript is a programming language';
 
-console.log(str.slice(-8));
+// let str = 'JavaScript is a programming language';
 
-console.log(str.substring(36, 28));
+// console.log(str.slice(-8));
 
-console.log(str.substr(28, 8));
+// console.log(str.substring(36, 28));
 
+// console.log(str.substr(28, 8));
+
+
+// let str = '$120';
+// check(str);
+// function check(str) {
+//     console.log(str.substr(1));
+// }
+
+
+// let str = 'добро пожаловать';
+// function upFL(str) {
+//     if (!str) {
+//         return str;
+//     } else {
+//         return str.substr(0, 1).toUpperCase() + str.substr(1);
+//     }
+// }
+// console.log(upFL(str));
+
+
+// let str = ' добро пожаловать ';
+// function upFL(str) {
+//     let nstr = str.trim();
+//     if (!nstr) {
+//         return nstr;
+//     } else {
+//         return nstr.substr(0, 1).toUpperCase() + nstr.substr(1);
+//     }
+// }
+// console.log(upFL(str));
+
+
+// let str = 'Домашний осел (лат.egegegeeveveve), или ишак - одомашненный подвид дикого осла.';
+// dBrackets(str);
+// function dBrackets(value) {
+//     let start = value.indexOf('(');
+//     let end = value.indexOf(')');
+//     let nValue = value.slice(0, start - 1) + value.slice(end + 1, value.length);
+//     console.log(nValue);
+// }
+
+
+// let str = 'Java is awesome. Java is fun.';
+// let oldStr = 'java';
+// let newStr = str.toLowerCase().replace(oldStr, 'JavaScript');
+// console.log(newStr);
+
+// newStr = str.toLowerCase().replaceAll(oldStr, 'JavaScript');
+// console.log(newStr);
+
+let str = '1-2-3-4-5';
+let newStr = str.replaceAll('-', '.')
+console.log(newStr);
