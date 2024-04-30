72febc5e490a2c74dcb6f11c479b903d0e1e7254
vladsrka <svv.post01@gmail.com>
2024-04-30 19:12:11 +0300

    work

--- Lesson-23/Lesson-23.1/script.js
+++ Lesson-23/Lesson-23.1/script.js
@@ -1,2 +1,13 @@
 "use strict";
+let str1 = 'http://dfgdfgdfvdv';
+check1();
+function check1() {
+    console.log(str1.startsWith('http://'));
+}
+
+let str2 = 'https://dfgdfgdfvdv';
+check2();
+function check2() {
+    console.log(str2.startsWith('https://'));
+}
 
