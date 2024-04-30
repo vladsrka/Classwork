a11939dbeda78231e24f8b82fafc6371cfbc0f84
vladsrka <svv.post01@gmail.com>
2024-04-30 19:37:58 +0300

    save

--- Lesson-23/Lesson-23.1/script.js
+++ Lesson-23/Lesson-23.1/script.js
@@ -1,13 +1,35 @@
 "use strict";
-let str1 = 'http://dfgdfgdfvdv';
-check1();
-function check1() {
-    console.log(str1.startsWith('http://'));
-}
+// let str1 = 'http://dfgdfgdfvdv';
+// check1(str1);
+// function check1(str11) {
+//     console.log(str11.startsWith('http://'));
+// }
 
-let str2 = 'https://dfgdfgdfvdv';
-check2();
-function check2() {
-    console.log(str2.startsWith('https://'));
+// let str2 = 'https://dfgdfgdfvdv';
+// check2(str2);
+// function check2(str22) {
+//     console.log(str22.startsWith('https://'));
+// }
+
+let str1 = 'https://dfgdfgdfvdv.com';
+let str2 = 'https://dfgdfgdfvdv.by';
+let str3 = 'https://dfgdfgdfvdv.ru';
+let str4 = 'https://dfgdfgdfvdv';
+check(str1);
+check(str2);
+check(str3);
+check(str4);
+function check(str) {
+    let line = str;
+    if (line.endsWith('.com') == true) {
+        console.log(`${line.endsWith('.com')} .com`);
+    } else if (line.endsWith('.by') == true) {
+        console.log(`${line.endsWith('.by')} .by`);
+    } else if (line.endsWith('.ru') == true) {
+        console.log(`${line.endsWith('.ru')} .ru`);
+    } else {
+        line = false;
+        console.log(line);
+    }
 }
 
