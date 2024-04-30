0f85584586f65ca4e2b185a024bd2ab8b73760bd
vladsrka <svv.post01@gmail.com>
2024-04-30 19:51:42 +0300

    save

--- Lesson-23/Lesson-23.1/script.js
+++ Lesson-23/Lesson-23.1/script.js
@@ -11,25 +11,33 @@
 //     console.log(str22.startsWith('https://'));
 // }
 
-let str1 = 'https://dfgdfgdfvdv.com';
-let str2 = 'https://dfgdfgdfvdv.by';
-let str3 = 'https://dfgdfgdfvdv.ru';
-let str4 = 'https://dfgdfgdfvdv';
-check(str1);
-check(str2);
-check(str3);
-check(str4);
-function check(str) {
-    let line = str;
-    if (line.endsWith('.com') == true) {
-        console.log(`${line.endsWith('.com')} .com`);
-    } else if (line.endsWith('.by') == true) {
-        console.log(`${line.endsWith('.by')} .by`);
-    } else if (line.endsWith('.ru') == true) {
-        console.log(`${line.endsWith('.ru')} .ru`);
-    } else {
-        line = false;
-        console.log(line);
-    }
-}
+// let str1 = 'https://dfgdfgdfvdv.com';
+// let str2 = 'https://dfgdfgdfvdv.by';
+// let str3 = 'https://dfgdfgdfvdv.ru';
+// let str4 = 'https://dfgdfgdfvdv';
+// check(str1);
+// check(str2);
+// check(str3);
+// check(str4);
+// function check(str) {
+//     let line = str;
+//     if (line.endsWith('.com') == true) {
+//         console.log(`${line.endsWith('.com')} .com`);
+//     } else if (line.endsWith('.by') == true) {
+//         console.log(`${line.endsWith('.by')} .by`);
+//     } else if (line.endsWith('.ru') == true) {
+//         console.log(`${line.endsWith('.ru')} .ru`);
+//     } else {
+//         line = false;
+//         console.log(line);
+//     }
+// }
+
+let str = 'JavaScript is a programming language';
+
+console.log(str.slice(-8));
+
+console.log(str.substring(36, 28));
+
+console.log(str.substr(28, 8));
 
