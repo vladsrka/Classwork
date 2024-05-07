14970f607bfcfe50a6c7c54608092f4059dd5dd6
vladsrka <svv.post01@gmail.com>
2024-05-07 18:56:28 +0300

    work

--- Lesson-23/Lesson-23.3/script.js
+++ Lesson-23/Lesson-23.3/script.js
@@ -1,2 +1,24 @@
 "use strict";
+// console.log('Задание 1');
+// let arr = ["груши", "яблоки", "сливы", "абрикосы", "апельсин"];
+// let newArr = [];
+// let lit = 'а';
+// for (let fruit of arr) {
+//     if (fruit.charAt(0) == lit) {
+//         newArr.push(fruit);
+//     }
+// }
+// console.log(newArr);
 
+// console.log('Задание 2');
+// let arr = ["груши", "яблоки", "сливы", "абрикосы", "апельсин"];
+// let newArr = [];
+// let lit = 5;
+// for (let fruit of arr) {
+//     if (fruit.length == lit) {
+//         newArr.push(fruit);
+//     }
+// }
+// console.log(newArr);
+
+console.log('Задание 3');
