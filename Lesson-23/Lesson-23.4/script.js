e6bb8e2a01f4c04cff3bace3de14b6f103270082
vladsrka <svv.post01@gmail.com>
2024-05-10 21:18:23 +0300

    work

--- Lesson-23/Lesson-23.4/script.js
+++ Lesson-23/Lesson-23.4/script.js
@@ -1,2 +1,20 @@
 "use strict";
+function filterHotels() {
+    const filters = document.querySelectorAll('.filters input[type="checkbox"]');
+    const cards = document.querySelectorAll('.card');
 
+    const selectedFilters = Array.from(filters).filter(filter => filter.checked).map(filter => filter.name);
+
+    cards.forEach(card => {
+        card.style.display = 'none';
+        const classes = card.className.split(' ');
+        if (selectedFilters.every(filter => classes.includes(filter))) {
+            card.style.display = 'block';
+        }
+    });
+}
+
+const filters = document.querySelectorAll('.filters input[type="checkbox"]');
+filters.forEach(filter => {
+    filter.addEventListener('change', filterHotels);
+});
